94e5507bf912daa0972be8148085ed0a9fd2123b
Vaibhav Kumar Singh <vaibhavksingh192@gmail.com>
2025-11-10 12:54:18 +0530

    push

---
+++ BookAPP/Book-app/component/Book.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './book.css'
+function Book() {
+  return (
+    <div id='book'>
+      <img src="https://imgv2-1-f.scribdassets.com/img/document/699284369/original/5c8022863c/1?v=1" height={150} width={150} alt="" />
+      <h1>Title:Math</h1>
+      <h1>250</h1>
+    </div>
+  )
+}
+
+export default Book
---
+++ BookAPP/Book-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Book from '../component/Book'
+import './App.css'
+
+function App() {
+  return (
+    <div id='vks'>
+      <Book />
+      <br />
+      <Book />
+      <br />
+      <Book />
+    </div>
+  )
+}
+
+export default App
